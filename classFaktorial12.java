dbb60e1fa1fe405bf874c29efe6dd911877b1b18
hunien <huniens06@gmail.com>
2024-03-23 20:55:56 +0700

    change

--- classFaktorial12.java
+++ classFaktorial12.java
@@ -8,18 +8,17 @@ public class classFaktorial12 {
     
     public int faktoria1BF(int n) {
     int fakto = 1;
-    for (int i = 1; i <= n; i++) {
-        fakto = fakto * i;
+    int i=1;
+    while (i <= n) {
+        fakto = fakto*i;
+        i++; 
     }
     return fakto;
-    }
-
+}
     public int faktoria1DC(int n) {
     if (n==1) {
         return 1;
-    }
-    else
-    {
+    }else{
         int fakto = n *faktoria1DC(n-1);
         return fakto;
     }
